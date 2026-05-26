e5bad50fe33989b5b496e8b3718b9b4e602386d0
yubowen <yubowen@kuaishou.com>
2026-05-26 16:55:48 +0800

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Layout from "@/components/layout";
 import Content from "@/modules/home/content";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Layout>
-      <Content />
-    </Layout>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Layout>
+        <Content />
+      </Layout>
+    </Suspense>
   );
 }
